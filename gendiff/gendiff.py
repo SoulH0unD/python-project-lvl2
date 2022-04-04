88c9d32285486a1affb39fc6947a39f14c80f332
Olemskoy Michael <desed@mail.ru>
2022-04-04 18:48:32 +0300

    step 3

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,7 +1,7 @@
 import json
 
 
-def generate_diff(file_path1: str, file_path2: str):#, format_name='stylish'):
+def generate_diff(file_path1: str, file_path2: str):#, format_name='stylish') -> str:
     source1 = json.load(open(file_path1))
     source2 = json.load(open(file_path2))
     keys = sorted(
@@ -12,20 +12,27 @@ def generate_diff(file_path1: str, file_path2: str):#, format_name='stylish'):
     differences = []
     for key in keys:
         if key in source1 and not(key in source2):
-            differences.append(f"- {key}: {source1[key]}")
+            differences.append(f" - {key}: {bool_to_str(source1[key])}")
 
         if not(key in source1) and key in source2:
-            differences.append(f"+ {key}: {source2[key]}")
+            differences.append(f" + {key}: {bool_to_str(source2[key])}")
 
         if key in source1 and key in source2:
             if source1[key] == source2[key]:
-                differences.append(f"  {key}: {source1[key]}")
+                differences.append(f"   {key}: {bool_to_str(source1[key])}")
             else:
-                differences.append(f"- {key}: {source1[key]}")
-                differences.append(f"+ {key}: {source2[key]}")
-
-
-    return differences
+                differences.append(f" - {key}: {bool_to_str(source1[key])}")
+                differences.append(f" + {key}: {bool_to_str(source2[key])}")
     
+    result = '{'
+    for value in differences:
+        result = result ='\n'.join([result, value.lower()])
 
-    #return source1#result
+    result ='\n'.join([result, '}'])
+    return result
+
+def bool_to_str(temp) -> str:
+    if type(temp)==bool:
+        return str(temp).lower()
+    return temp
+
